61f5e61b3b42c435fb311ea54a474a4ff7925a6a
renatex333 <renato9302@hotmail.com>
2023-09-15 15:18:21 -0300

    Finishing Tutotial

--- Rall-U-Ca/Assets/Scripts/PlayerController.cs
+++ Rall-U-Ca/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
 
@@ -15,6 +19,10 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
        rb = GetComponent<Rigidbody>(); 
+       count = 0;
+
+       SetCountText();
+       winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -24,9 +32,29 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+
+            SetCountText();
+        }
     }
 }

3f8f7a532ac230d9313c84b2b5af8a14bf767e22
Renato Laffranchi Falcão <66042074+renatex333@users.noreply.github.com>
2023-09-17 12:08:17 -0300

    Items instancing and positioning fixed

--- ShabooyaRoll/Assets/Scripts/ItemManager.cs
+++ ShabooyaRoll/Assets/Scripts/ItemManager.cs
@@ -2,42 +2,51 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random=UnityEngine.Random;
+using Debug=UnityEngine.Debug;
 
 public class ItemManager : MonoBehaviour
 {
     public GameObject Prefab;
-    public int NumberOfObjects;
-    // private Vector3[] _positions;
-
+    public int numberOfObjects;
+    private Vector3[] positions;
 
     // This script will simply instantiate the Prefab when the game starts.
     void Start()
     {
-        // _positions = new[]{
-        //     new Vector3( -45, 0.5f,  45),
-        //     new Vector3(  35, 0.5f,  45),
-        //     new Vector3( -45, 0.5f, -45),
-        //     new Vector3(  45, 0.5f, -45),
-        //     new Vector3( -15, 0.5f,  45),
-        //     new Vector3(  15, 0.5f,  45),
-        //     new Vector3( -35, 0.5f,  -5),
-        //     new Vector3(  45, 0.5f, -25),
-        //     new Vector3(  45, 0.5f,   5),
-        //     new Vector3( -25, 0.5f, -45),
-        //     new Vector3(   5, 0.5f, -35),
-        //     new Vector3( -45, 0.5f,  35),
-        //     new Vector3(   5, 0.5f,  25),
-        //     new Vector3( -15, 0.5f, -25),
-        //     new Vector3(  25, 0.5f,  -5),
-        //     new Vector3( -25, 0.5f,  45),
-        //     new Vector3(  35, 0.5f,  25),
-        // };
-        // for (int i = 0; i < NumberOfObjects; i++)
-        // {
-        //     Instantiate(Prefab, _positions[i], Quaternion.identity);
-        // }
-        // Tamanho dos treco errado e posição errada
-        Instantiate(Prefab, new Vector3( -45, 0.5f,  45), Quaternion.identity);
+        positions = new[]{
+            new Vector3( -45, 0.5f,  45),
+            new Vector3(  35, 0.5f,  45),
+            new Vector3( -45, 0.5f, -45),
+            new Vector3(  45, 0.5f, -45),
+            new Vector3( -15, 0.5f,  45),
+            new Vector3(  15, 0.5f,  45),
+            new Vector3( -35, 0.5f,  -5),
+            new Vector3(  45, 0.5f, -25),
+            new Vector3(  45, 0.5f,   5),
+            new Vector3( -25, 0.5f, -45),
+            new Vector3(   5, 0.5f, -35),
+            new Vector3( -45, 0.5f,  35),
+            new Vector3(   5, 0.5f,  25),
+            new Vector3( -15, 0.5f, -25),
+            new Vector3(  25, 0.5f,  -5),
+            new Vector3( -25, 0.5f,  45),
+            new Vector3(  35, 0.5f,  25),
+        };
+
+        HashSet<int> indexes = new HashSet<int>();
+        int minValue = 0;
+        int maxValue = positions.Length;
+
+        while (indexes.Count < numberOfObjects)
+        {
+            indexes.Add(Random.Range(minValue, maxValue));
+        }
+        
+        foreach(int i in indexes)
+        {
+            Instantiate(Prefab, positions[i] * 3, Quaternion.identity);
+        }
         
     }
 }
